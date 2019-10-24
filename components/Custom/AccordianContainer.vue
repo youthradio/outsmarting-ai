e3b662cef6cc1634afc49987eb31af3538b8a84f
shagia <mrkraiola@gmail.com>
2019-10-24 14:48:52 -0700

    Add AccordianContainer component

---
+++ components/Custom/AccordianContainer.vue
@@ -0,0 +1,94 @@
+<template>
+  <article class="accordian" :class="accordianClasses">
+    <div class="accordian-header" @click="toggleAccordion">
+      {{ accordianTitle }} <span class="accordianIcon" :class="accordianClasses" />
+    </div>
+    <div class="accordian-body">
+      <div class="accordian-content">
+        Test Content but much longer and filling up the space in the component
+      </div>
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  props: {
+    accordianTitle: {
+      type: String,
+      require: false,
+      default: null
+    },
+    accordianBody: {
+      type: String,
+      require: true,
+      default: null
+    }
+  },
+  data () {
+    return {
+      isOpen: !true
+    }
+  },
+  computed: {
+    accordianClasses () {
+      return {
+        'is-closed': !this.isOpen,
+        'is-primary': this.isOpen,
+        'is-dark': !this.isOpen
+      }
+    }
+  },
+  methods: {
+    toggleAccordion () {
+      this.isOpen = !this.isOpen
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~@/css/mixins';
+@import "~@/css/vars";
+.accordian{
+    background-color: #00EBAB;
+    color: black;
+    padding: 0 0.3rem 0.3rem 0.3rem; // actually is just the content's border
+    max-width: 100%;
+}
+
+.accordian-header{
+  cursor: pointer;
+  display: flex;
+  justify-content: space-between;
+  padding: 0.4rem;
+}
+
+.is-closed .accordianIcon:after{
+    content: '▾';
+    position: relative;
+    pointer-events: none;
+}
+
+.is-primary .accordianIcon:after{
+    content: '▴';
+    position: relative;
+    pointer-events: none;
+}
+
+.is-closed .accordian-body{
+  max-height: 0rem;
+}
+
+.is-primary .accordian-body{
+    padding: 0.3rem;
+}
+
+.accordian-body{
+    max-height: 10rem;
+    background-color: $white;
+    overflow-y: scroll;
+    overflow-x: hidden;
+    transition: 0.2s ease all;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,12 @@
   <div class="container">
     <HeaderContainer />
     <article>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mus mauris vitae ultricies leo integer. Odio ut sem nulla pharetra. Mauris augue neque gravida in fermentum et. Placerat duis ultricies lacus sed. Proin fermentum leo vel orci porta non pulvinar neque laoreet. Maecenas ultricies mi eget mauris. Malesuada fames ac turpis egestas integer eget aliquet nibh. Urna et pharetra pharetra massa massa ultricies. Interdum velit laoreet id donec.</p>
+      <AccordianContainer :accordian-title="articleData.sections[0].title" />
+      <AccordianContainer :accordian-title="articleData.sections[1].title" />
+      <AccordianContainer :accordian-title="articleData.sections[2].title" />
+      <p v-html="articleData.leading" />
+      <p v-html="articleData.leading2" />
+      <p v-html="articleData.leading3" />
       <p>Vitae purus faucibus ornare suspendisse sed nisi. Dignissim cras tincidunt lobortis feugiat vivamus at. Lorem mollis aliquam ut porttitor. Mauris in aliquam sem fringilla ut morbi tincidunt augue. Lorem ipsum dolor sit amet. Nibh mauris cursus mattis molestie a iaculis at. Vulputate sapien nec sagittis aliquam malesuada bibendum. Viverra suspendisse potenti nullam ac tortor vitae purus faucibus. Interdum consectetur libero id faucibus nisl tincidunt eget. Vulputate enim nulla aliquet porttitor lacus luctus. Ornare arcu dui vivamus arcu felis bibendum ut tristique. Dignissim cras tincidunt lobortis feugiat vivamus at. Turpis massa tincidunt dui ut ornare lectus.</p>
       <p>Lobortis elementum nibh tellus molestie nunc non blandit massa. Elit duis tristique sollicitudin nibh sit amet commodo nulla facilisi. Semper eget duis at tellus at urna. Sed faucibus turpis in eu. Sit amet volutpat consequat mauris nunc. Libero volutpat sed cras ornare arcu dui. Ut pharetra sit amet aliquam id. Turpis egestas pretium aenean pharetra magna ac placerat vestibulum. Mi proin sed libero enim sed faucibus turpis in eu. Dui accumsan sit amet nulla facilisi morbi tempus. Leo a diam sollicitudin tempor. Nunc mi ipsum faucibus vitae aliquet nec ullamcorper sit amet. Consectetur libero id faucibus nisl tincidunt. In ornare quam viverra orci sagittis. Morbi tincidunt augue interdum velit euismod in pellentesque massa. Dolor sit amet consectetur adipiscing. Quisque id diam vel quam elementum pulvinar etiam non quam. Pellentesque elit eget gravida cum sociis natoque penatibus. Urna neque viverra justo nec ultrices dui sapien. Sed id semper risus in hendrerit gravida rutrum.</p>
       <MapContainer />
@@ -17,10 +22,12 @@
 
 <script>
 
+import ArticleData from '../data/data.json'
 import CommonUtils from '../mixins/CommonUtils'
 import RelatedPostsContainer from '~/components/RelatedPosts/RelatedPostsContainer'
 import MapContainer from '~/components/Map/MapContainer'
 import HeaderContainer from '~/components/Header/HeaderContainer'
+import AccordianContainer from '~/components/Custom/AccordianContainer'
 import ShareContainer from '~/components/custom/ShareContainer'
 import FooterContainer from '~/components/Footer/FooterContainer'
 
@@ -30,21 +37,26 @@ export default {
     MapContainer,
     HeaderContainer,
     ShareContainer,
-    FooterContainer
+    FooterContainer,
+    AccordianContainer
   },
   mixins: [
     CommonUtils
   ],
   data () {
     return {
+
     }
   },
-  computed: {},
+  computed: {
+  },
   watch: {
 
   },
-  async asyncData (ctx) {
-
+  asyncData (ctx) {
+    return {
+      articleData: ArticleData.content
+    }
   },
   mounted () {
   },
